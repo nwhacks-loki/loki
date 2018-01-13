aa2d499adbabecc94223b40d74bedabf5b45a958
Nathan Tannar <nathantannar4@gmail.com>
2018-01-13 14:54:27 -0800

    Project Setup

---
+++ Source/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nwHacks2018
+//
+//  Created by Nathan Tannar on 1/13/18.
+//  Copyright © 2018 Nathan Tannar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
