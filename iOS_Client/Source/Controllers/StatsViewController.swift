fdd06c6c67f508d971994427a8076c48c916a3b7
Nathan Tannar <nathantannar4@gmail.com>
2018-01-14 02:11:07 -0800

    Undo Charts

--- iOS_Client/Source/Controllers/StatsViewController.swift
+++ iOS_Client/Source/Controllers/StatsViewController.swift
@@ -7,4 +7,5 @@
 //
 
 import UIKit
-import Charts
+//import Charts
+
--- iOS_Client/Source/Views/FeedCell.swift
+++ iOS_Client/Source/Views/FeedCell.swift
@@ -14,4 +14,12 @@ class FeedCell: UICollectionViewCell {
         return "FeedCell"
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
